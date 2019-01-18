b6df88c1c2dc365fe710af27376fef314edad3e1
Vpugh <taws26@gmail.com>
2019-01-18 00:47:49 -0500

    Added nextRound and isEven functions

--- src/components/timer/timer.jsx
+++ src/components/timer/timer.jsx
@@ -14,7 +14,6 @@ class Timer extends React.Component {
       restTime: 10, // rest between each exercise
       totalWorkoutTime: '', // total amount of workout - # exercies x amt of rounds
       totalRestTime: '', // total rest in workout - time of rest x (amt of rounds - 1)
-      interval: null, // 
       currentRound: 0, // current exercise round
       totalStages: [],
       totalExercises: [],
@@ -25,6 +24,7 @@ class Timer extends React.Component {
     this.timerPause = this.timerPause.bind(this);
     this.timerReset = this.timerReset.bind(this);
     this.countdown = this.countdown.bind(this);
+    this.isEven = this.isEven.bind(this);
   }
 
   componentDidMount() {
@@ -33,12 +33,6 @@ class Timer extends React.Component {
     this.createStages();
     this.createExercise();
   }
-
-  // componentDidUpdate(prevState) {
-  //   if (prevState.exerciseTime !== this.state.exerciseTime) {
-  //     this.setState({ totalWorkoutTime: this.state.exerciseTime })
-  //   }
-  // }
   
   // Generate rounds and Exercise Arrays
   createStages() {
@@ -71,22 +65,30 @@ class Timer extends React.Component {
     }
   }
 
+  isEven(num) {
+    return num % 2 === 0;
+  }
+
   // Timer Controls
 
   timerPlay() {
+    console.log('Play');
     this.setState({
       isTimerRunning: true,
       isTimerPaused: false,
-      interval: setInterval(this.countdown(), 1000),
     });
+    this.interval = setInterval(
+      () => this.countdown(),
+      1000
+    );
   }
 
   timerPause() {
+    console.log('Paused');
     this.setState({
       isTimerRunning: false,
       isTimerPaused: true,
-      interval: null,
-    }, clearInterval(this.state.interval));
+    }, clearInterval(this.interval));
   }
   
   timerReset() {
@@ -95,37 +97,59 @@ class Timer extends React.Component {
       isTimerPaused: false,
       totalWorkoutTime: this.state.exerciseTime,
       round: 0,
-      interval: null,
-    }, clearInterval(this.state.interval));
+    }, clearInterval(this.interval));
   }
 
   countdown() {
-    const { totalWorkoutTime, interval, currentRound } = this.state;
+    const { totalWorkoutTime, currentRound } = this.state;
     if (totalWorkoutTime === 0 && this.roundTotal === currentRound) {
       console.log('Next Round');
       this.setState({
         isTimerRunning: false,
         isTimerPaused: false,
-        interval: null,
-      }, clearInterval(interval));
+      }, clearInterval(this.interval));
       this.nextRound();
     } else if (totalWorkoutTime === 0 && this.roundTotal !== currentRound) {
       console.log('Next Round 2');
-      clearInterval(interval);
-      this.setState({
-        interval: setInterval(this.countdownTimer(), 1000),
-      }, this.nextRound());
-      setInterval(this.countdown(), 1000);
+      clearInterval(this.interval);
+      this.nextRound();
+      this.interval = setInterval(
+        () => this.countdown(),
+        1000
+      );
     } else if ( totalWorkoutTime <= 6 && totalWorkoutTime !== 0) {
       console.log('Countdown---')
-      clearInterval(interval);
+      clearInterval(this.interval);
       this.setState({
-        interval: setInterval(this.countdownTimer(), 1000),
         totalWorkoutTime: +totalWorkoutTime - 1,
-      }, setInterval(this.countdown(), 1000));
+      });
+      this.interval = setInterval(
+        () => this.countdown(),
+        1000
+      );
     } else {
       this.setState({
-        totalWorkoutTime: +totalWorkoutTime - 1,
+        totalWorkoutTime: totalWorkoutTime - 1,
+      });
+    }
+  }
+
+  nextRound() {
+    const { currentRound, restTime, totalRounds, exerciseTime } = this.state;
+    this.setState({
+      currentRound: currentRound + 1,
+    });
+    if (currentRound === totalRounds) {
+      this.timerReset();
+    }
+    if (this.isEven(currentRound)) {
+      this.setState({
+        totalWorkoutTime: exerciseTime,
+      });
+    }
+    if (!this.isEven(currentRound)) {
+      this.setState({
+        totalWorkoutTime: restTime,
       });
     }
   }
@@ -152,7 +176,6 @@ class Timer extends React.Component {
           exercises={exercises}
           totalExercises={totalExercises}
         />
-        {this.state.interval}
         <div className="timer">{this.exerciseRoundTime()}</div>
         <div className="buttons">
           {!isTimerRunning && <button onClick={this.timerPlay}>Start <span className="fa fa-play"></span></button>}
